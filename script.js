2f2f0de70e4a73cc4827c50fd42bcfc6ea57e326
Onkar Sutar <onkarsutar4939@gmail.com>
2024-10-02 07:22:07 +0530

    Add a file

---
+++ script.js
@@ -0,0 +1,28 @@
+let login = document.getElementById("loginform");
+        let signup = document.getElementById("signup");
+        let btns = document.getElementById("s");
+        let btnl = document.getElementById("l");
+
+
+
+        function Login() {
+            login.style.display = "block";
+            signup.style.display = "none";
+            btns.style.display = "none";
+            btnl.style.display = "block";
+            btnl.style.margin = "20px 180px"
+
+            // btnl.style.textAlign = "center"
+
+
+        }
+        function SignUp() {
+            login.style.display = "none";
+            signup.style.display = "block";
+            btnl.style.display = "none";
+            btns.style.display = "block";
+            btns.style.margin = "20px 160px"
+            // btns.style.textAlign = "center"
+
+
+        }
